d2bfd17ba05977e66c28cad5087d92a9f8d82a32
Lost <ry4n.noliveirasc@gmail.com>
2025-10-18 02:06:21 -0300

    initial git settings

---
+++ modules/homeManager/git.nix
@@ -0,0 +1,17 @@
+{ config, pkgs, ... }:
+
+let
+    # Read email from file
+    userEmail = builtins.trim (builtins.readFile ./email.txt);
+in
+
+{
+    programs.git = {
+    enable = true;
+    userName = "Lost";
+    userEmail = userEmail;
+    extraConfig = {
+        init.defaultBranch = "main";
+    };
+    };
+}
