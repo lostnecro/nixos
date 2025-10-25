c8668321efed0afbca141b288364b34d050c6e65
Lost <ry4n.noliveirasc@gmail.com>
2025-10-24 23:17:54 -0300

    replace logo source with ASCII art and remove unused PNG file

--- modules/homeManager/fastfetch.nix
+++ modules/homeManager/fastfetch.nix
@@ -5,9 +5,28 @@
         enable = true;
         settings = {
             logo = {
-                type = "kitty";
-                source = "/etc/nixos/modules/homeManager/logo.png";
+                #type = "kitty";
+                source = "/etc/nixos/modules/homeManager/ascii.txt";
             };
+            padding = 2;
+            modules = [
+                "os"
+                "host"
+                "kernel"
+                "datetime"
+                "uptime"
+                "break"
+                "packages"
+                "shell"
+                "memory"
+                "disk"
+                "battery"
+                "cpu"
+                "cpu_usage"
+                "gpu"
+                "network"
+                "locale"
+                ];
         };
     };
 }
